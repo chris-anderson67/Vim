927c810663f84cf35c0cdb45a27b57b0e3d44913
Chris Anderson <chrisja67@gmail.com>
2022-02-20 20:39:29 -0500

    Auto fetch vim-plug setup script

--- setup-vim-plug.sh
+++ setup-vim-plug.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
- ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
